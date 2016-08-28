1896fbf21fbb5eeebdaa377314759e14012b8289
d12frosted <boris@d12frosted.io>
2016-08-28 17:00:34 +0300

    allow to override certain path in update script

--- update.sh
+++ update.sh
@@ -1,13 +1,35 @@
 #!/usr/bin/env bash
 
+if [ -z "${1+x}" ]; then
+  MIRROR_PATH=$(pwd)
+else
+  MIRROR_PATH=$1
+fi
+
+if [ -z "${2+x}" ]; then
+  LOGFILE=$MIRROR_PATH/output.log
+else
+  LOGFILE=$2
+fi
+
+if [ -z "${3+x}" ]; then
+  ELPA_CLONE_PATH="$HOME/elpa-clone"
+else
+  ELPA_CLONE_PATH=$3
+fi
+
 ELPA_CLONE_URL="git@github.com:dochang/elpa-clone.git"
-ELPA_CLONE_PATH="$HOME/elpa-clone"
-OUTPUT_PATH=$(pwd)
+
+echo "Starting..."
+echo "Output to: $MIRROR_PATH"
+echo "elpa-clone in: $ELPA_CLONE_PATH"
 
 set -e
 
+cd "$MIRROR_PATH"
+
 function log {
-  echo "[$(date '+%d/%m/%y %H:%M:%S')]" "$@" >> "$OUTPUT_PATH/output.log"
+  echo "[$(date '+%d/%m/%y %H:%M:%S')]" "$@" >> "$LOGFILE"
 }
 
 log "Start updating elpa mirrors"
@@ -18,7 +40,7 @@ fi
 
 function clone {
   log "Updating mirror for $2 ($1)"
-  emacs -l "$ELPA_CLONE_PATH/elpa-clone.el" -nw --batch --eval="(elpa-clone \"$1\" \"$OUTPUT_PATH/$2\")" 2>> "$OUTPUT_PATH/output.log"
+  emacs -l "$ELPA_CLONE_PATH/elpa-clone.el" -nw --batch --eval="(elpa-clone \"$1\" \"$MIRROR_PATH/$2\")" 2>> "$LOGFILE"
 }
 
 clone "http://orgmode.org/elpa/" "org"
