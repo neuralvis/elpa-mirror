87ffb8e26e7e9fef22b1a0a1d334ed1034dbeccb
Golem <golem@d12frosted.io>
2017-10-13 16:00:07 +0000

    snapshot 13/10/17 16:00:07

--- melpa/pocket-reader-20171013.250.el
+++ melpa/pocket-reader-20171013.250.el
@@ -5,7 +5,7 @@
 ;; Author: Adam Porter <adam@alphapapa.net>
 ;; Created: 2017-09-25
 ;; Version: 0.1-pre
-;; Package-Version: 20171013.19
+;; Package-Version: 20171013.250
 ;; Keywords: pocket
 ;; Package-Requires: ((emacs "25.1") (dash "2.13.0") (kv "0.0.19") (pocket-lib "0.1") (s "1.10") (ov "1.0.6") (rainbow-identifiers "0.2.2") (org-web-tools "0.1"))
 ;; URL: https://github.com/alphapapa/pocket-reader.el
@@ -600,7 +600,15 @@ Chooses URL fields as configured by `pocket-reader-url-priorities'."
   "Add and display ITEMS."
   (setq pocket-reader-items (append pocket-reader-items items))
   (pocket-reader--set-tabulated-list-format)
-  (setq tabulated-list-entries pocket-reader-items)
+
+  ;; Use a copy of the list.  Otherwise, when the tabulated list is sorted, `pocket-reader-items'
+  ;; gets rearranged when `tabulated-list-entries' gets sorted, and that somehow causes the apparent
+  ;; length of `pocket-reader-items' to change, and that causes items to disappear from the list
+  ;; when `pocket-reader-more' is called.  This is a very strange bug, but it's basically caused by
+  ;; `sort' modifying lists by side effects.  Making `tabulated-list-entries' a copy avoids this
+  ;; problem while allowing them to share the underlying items, which aren't changed.
+  (setq tabulated-list-entries (copy-sequence pocket-reader-items))
+
   (tabulated-list-init-header)
   (tabulated-list-revert)
   (pocket-reader--finalize))
