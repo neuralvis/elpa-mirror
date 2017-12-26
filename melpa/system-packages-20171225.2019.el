27b85d10998f9ca02b05d913520ef75873f58bbb
Golem <golem@d12frosted.io>
2017-12-26 20:00:05 +0000

    snapshot 26/12/17 20:00:05

--- melpa/system-packages-20171225.2019.el
+++ melpa/system-packages-20171225.2019.el
@@ -5,8 +5,7 @@
 ;; Author: J. Alexander Branham <branham@utexas.edu>
 ;; Maintainer: J. Alexander Branham <branham@utexas.edu>
 ;; URL: https://github.com/jabranham/system-packages
-;; Package-Version: 20171216.934
-;; Version: 0.1
+;; Package-Version: 20171225.2019
 ;; Package-Requires: ((cl-lib "0.5"))
 
 ;; This file is not part of GNU Emacs.
@@ -88,24 +87,6 @@
            (list-dependencies-of . "port deps")
            (noconfirm . nil)))
     ;; Arch-based systems
-    (pacaur .
-            ((default-sudo . nil)
-             (install . "pacaur -S")
-             (search . "pacaur -Ss")
-             (uninstall . "pacaur -Rs")
-             (update . "pacaur -Syu")
-             (clean-cache . "pacaur -Sc")
-             (log . "cat /var/log/pacman.log")
-             (get-info . "pacaur -Qi")
-             (get-info-remote . "pacaur -Si")
-             (list-files-provided-by . "pacaur -Ql")
-             (verify-all-packages . "pacaur -Qkk")
-             (verify-all-dependencies . "pacaur -Dk")
-             (remove-orphaned . "pacaur -Rns $(pacman -Qtdq)")
-             (list-installed-packages . "pacaur -Qe")
-             (list-installed-packages-all . "pacaur -Q")
-             (list-dependencies-of . "pacaur -Qi")
-             (noconfirm . "--noconfirm")))
     (pacman .
             ((default-sudo . t)
              (install . "pacman -S")
@@ -240,12 +221,12 @@
                    (list-dependencies-of . "xbps-query -x")
                    (noconfirm . nil)))))
 
-(defcustom system-packages-packagemanager
+(defcustom system-packages-package-manager
   (cl-loop for (name . prop) in system-packages-supported-package-managers
            for path = (executable-find (symbol-name name))
            when path
            return name)
-  "String containing the package manager to use.
+  "Symbol containing the package manager to use.
 
 See `system-packages-supported-package-managers' for a list of
 supported software.  Tries to be smart about selecting the
@@ -253,13 +234,19 @@ default."
   :group 'system-packages
   :type 'symbol)
 
-(defcustom system-packages-usesudo
+(define-obsolete-variable-alias 'system-packages-packagemanager
+  'system-packages-package-manager "2017-12-25")
+
+(defcustom system-packages-use-sudo
   (cdr (assoc 'default-sudo (cdr (assoc system-packages-packagemanager
                                         system-packages-supported-package-managers))))
   "If non-nil, system-packages uses sudo for appropriate commands.
 
 Tries to be smart for selecting the default."
   :group 'system-packages)
+
+(define-obsolete-variable-alias 'system-packages-usesudo
+  'system-packages-use-sudo "2017-12-25")
 
 (defcustom system-packages-noconfirm nil
   "If non-nil, bypass prompts asking the user to confirm package upgrades."
