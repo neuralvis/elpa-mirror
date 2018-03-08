971ed6aa428c0e00875f2910277355b22b857e97
Golem <golem@d12frosted.io>
2018-03-08 12:00:10 +0000

    snapshot 08/03/18 12:00:10

--- gnu/posframe-0.2.0.el
+++ gnu/posframe-0.2.0.el
@@ -5,8 +5,7 @@
 ;; Author: Feng Shu <tumashu@163.com>
 ;; Maintainer: Feng Shu <tumashu@163.com>
 ;; URL: https://github.com/tumashu/posframe
-;; Package-Version: 20180306.2358
-;; Version: 0.1.0
+;; Version: 0.2.0
 ;; Keywords: tooltip
 ;; Package-Requires: ((emacs "26"))
 
@@ -141,7 +140,9 @@ frame.")
                                      right-fringe
                                      font
                                      keep-ratio
-                                     override-parameters)
+                                     override-parameters
+                                     respect-header-line
+                                     respect-mode-line)
   "Create a child-frame for posframe.
 This posframe's buffer is POSFRAME-BUFFER."
   (let ((left-fringe (or left-fringe 0))
@@ -155,18 +156,22 @@ This posframe's buffer is POSFRAME-BUFFER."
                     left-fringe
                     font
                     keep-ratio
-                    override-parameters)))
+                    override-parameters
+                    respect-header-line
+                    respect-mode-line)))
     (with-current-buffer posframe-buffer
       ;; Many variables take effect after call `set-window-buffer'
       (setq-local left-fringe-width nil)
       (setq-local right-fringe-width nil)
       (setq-local fringes-outside-margins 0)
       (setq-local truncate-lines t)
-      (setq-local mode-line-format nil)
-      (setq-local header-line-format nil)
       (setq-local cursor-type nil)
       (setq-local cursor-in-non-selected-windows nil)
       (setq-local show-trailing-whitespace nil)
+      (unless respect-mode-line
+        (setq-local mode-line-format nil))
+      (unless respect-header-line
+        (setq-local header-line-format nil))
 
       ;; Create child-frame
       (unless (and (frame-live-p posframe--frame)
@@ -190,7 +195,8 @@ This posframe's buffer is POSFRAME-BUFFER."
                           (cons 'font font))
                        (parent-frame . ,(or parent-frame (window-frame)))
                        (keep-ratio ,keep-ratio)
-                       (posframe-buffer . ,posframe-buffer)
+                       (posframe-buffer . ,(cons (buffer-name posframe-buffer)
+                                                 posframe-buffer))
                        (no-accept-focus . t)
                        (min-width  . 0)
                        (min-height . 0)
@@ -217,8 +223,10 @@ This posframe's buffer is POSFRAME-BUFFER."
                        (desktop-dont-save . t))))
         (let ((posframe-window (frame-root-window posframe--frame)))
           ;; This method is more stable than 'setq mode/header-line-format nil'
-          (set-window-parameter posframe-window 'mode-line-format 'none)
-          (set-window-parameter posframe-window 'header-line-format 'none)
+          (unless respect-mode-line
+            (set-window-parameter posframe-window 'mode-line-format 'none))
+          (unless respect-header-line
+            (set-window-parameter posframe-window 'header-line-format 'none))
           (set-window-buffer posframe-window posframe-buffer)))
       posframe--frame)))
 
@@ -238,6 +246,8 @@ This posframe's buffer is POSFRAME-BUFFER."
                          font
                          foreground-color
                          background-color
+                         respect-header-line
+                         respect-mode-line
                          no-properties
                          keep-ratio
                          override-parameters
@@ -295,6 +305,10 @@ By default, posframe's foreground and background color are
 deriverd from current frame, user can set them with the help
 of FOREGROUND-COLOR and BACKGROUND-COLOR.
 
+By default, posframe will force hide header-line and mode-line
+If user want to show header-line or mode-line in posframe,
+set RESPECT-HEADER-LINE or RESPECT-MODE-LINE to t.
+
 OVERRIDE-PARAMETERS is very powful, *all* the frame parameters
 used by posframe's frame can be overrided by it.
 
@@ -335,6 +349,8 @@ you can use `posframe-delete-all' to delete all posframes."
              :foreground-color foreground-color
              :background-color background-color
              :keep-ratio keep-ratio
+             :respect-header-line respect-header-line
+             :respect-mode-line respect-mode-line
              :override-parameters override-parameters))
 
       ;; Insert string to posframe-buffer.
@@ -468,9 +484,11 @@ WIDTH and MIN-WIDTH."
 
 (defun posframe-hide (posframe-buffer)
   "Hide posframe which buffer is POSFRAME-BUFFER."
-  (with-current-buffer (get-buffer-create posframe-buffer)
-    (when (frame-live-p posframe--frame)
-      (make-frame-invisible posframe--frame))))
+  (dolist (frame (frame-list))
+    (let ((buffer-info (frame-parameter frame 'posframe-buffer)))
+      (when (or (equal posframe-buffer (car buffer-info))
+                (equal posframe-buffer (cdr buffer-info)))
+        (make-frame-invisible frame)))))
 
 (defun posframe-delete (posframe-buffer)
   "Delete posframe which buffer POSFRAME-BUFFER."
@@ -481,13 +499,16 @@ WIDTH and MIN-WIDTH."
   "Kill child-frame of posframe.
 This posframe's buffer is POSFRAME-BUFFER."
   (dolist (frame (frame-list))
-    (let ((buffer (frame-parameter frame 'posframe-buffer)))
-      (when (equal posframe-buffer buffer)
-        (with-current-buffer posframe-buffer
-          (dolist (timer '(posframe--refresh-timer
-                           posframe--timeout-timer))
-            (when (timerp timer)
-              (cancel-timer timer))))
+    (let ((buffer-info (frame-parameter frame 'posframe-buffer))
+          (buffer (get-buffer posframe-buffer)))
+      (when (or (equal posframe-buffer (car buffer-info))
+                (equal posframe-buffer (cdr buffer-info)))
+        (when buffer
+          (with-current-buffer buffer
+            (dolist (timer '(posframe--refresh-timer
+                             posframe--timeout-timer))
+              (when (timerp timer)
+                (cancel-timer timer)))))
         (delete-frame frame)))))
 
 (defun posframe--kill-buffer (posframe-buffer)
@@ -499,18 +520,17 @@ This posframe's buffer is POSFRAME-BUFFER."
 (defun posframe-hide-all ()
   "Hide all posframe's frames."
   (interactive)
-  (dolist (buffer (buffer-list))
-    (with-current-buffer buffer
-      (when (frame-live-p posframe--frame)
-        (make-frame-invisible posframe--frame)))))
+  (dolist (frame (frame-list))
+    (let ((buffer-info (frame-parameter frame 'posframe-buffer)))
+      (when buffer-info (make-frame-invisible frame)))))
 
 ;;;###autoload
 (defun posframe-delete-all ()
   "Delete all posframe's frames and buffers."
   (interactive)
   (dolist (frame (frame-list))
-    (let ((buffer (frame-parameter frame 'posframe-buffer)))
-      (when buffer (delete-frame frame))))
+    (let ((buffer-info (frame-parameter frame 'posframe-buffer)))
+      (when buffer-info (delete-frame frame))))
   (dolist (buffer (buffer-list))
     (with-current-buffer buffer
       (when posframe--frame
@@ -520,10 +540,7 @@ This posframe's buffer is POSFRAME-BUFFER."
   "Auto delete posframe when its buffer is killed.
 
 This function is used by `kill-buffer-hook'."
-  (dolist (frame (frame-list))
-    (let ((buffer (frame-parameter frame 'posframe-buffer)))
-      (when (eq buffer (current-buffer))
-        (delete-frame frame)))))
+  (posframe--delete-frame (current-buffer)))
 
 (add-hook 'kill-buffer-hook #'posframe-auto-delete)
 
@@ -721,6 +738,31 @@ docstring of `posframe-show'."
              (- 0 posframe-width))
           (+ window-top window-height
              (- 0 modeline-height posframe-height)))))
+
+;;;; ChangeLog:
+
+;; 2018-03-08  Feng Shu  <tumashu@163.com>
+;; 
+;; 	Merge commit 'a8c24d676c6e1a47ea4b010633a31571d59f506f'
+;; 
+;; 2018-02-27  Stefan Monnier  <monnier@iro.umontreal.ca>
+;; 
+;; 	Add 'packages/posframe/' from commit
+;; 	'19b66bc26e39527934ba8a26c6443cead15b7614'
+;; 
+;; 	git-subtree-dir: packages/posframe git-subtree-mainline:
+;; 	9d93270481f2c5f4f9c78f11eac8d410af683d1c git-subtree-split:
+;; 	19b66bc26e39527934ba8a26c6443cead15b7614
+;; 
+;; 2018-02-27  Stefan Monnier  <monnier@iro.umontreal.ca>
+;; 
+;; 	Preparing to redo the subtree add
+;; 
+;; 2018-02-27  Stefan Monnier  <monnier@iro.umontreal.ca>
+;; 
+;; 	Fix "rebase" mess up
+;; 
+
 
 (provide 'posframe)
 
