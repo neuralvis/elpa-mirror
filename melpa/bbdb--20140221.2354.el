b66342cfd6c28cc9a9b4af477aea23681a4265e1
Golem <golem@d12frosted.io>
2018-09-15 16:09:18 +0000

    snapshot 15/09/18 16:09:18

--- melpa/bbdb--20140221.2354.el
+++ melpa/bbdb--20140221.2354.el
@@ -4,6 +4,7 @@
 
 ;; Author: Hiroaki Otsu <ootsuhiroaki@gmail.com>
 ;; Keywords: bbdb, news, mail
+;; Package-Version: 20140221.2354
 ;; URL: https://github.com/aki2o/bbdb-
 ;; Package-Requires: ((bbdb "20140123.1541") (log4e "0.2.0") (yaxception "0.1"))
 ;; Version: 0.0.2
@@ -974,6 +975,4 @@
 
 
 (provide 'bbdb-)
-;;; bbdb-.el ends here
-de 'bbdb-)
 ;;; bbdb-.el ends here
