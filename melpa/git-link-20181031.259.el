a35c1b218ed750a10c162bf0f4afe6b420b6bad9
Golem <golem@d12frosted.io>
2018-10-31 08:46:36 +0000

    snapshot 31/10/18 08:46:35

--- melpa/git-link-20181031.259.el
+++ melpa/git-link-20181031.259.el
@@ -2,8 +2,8 @@
 
 ;; Copyright (C) 2013-2017 Skye Shaw and others
 ;; Author: Skye Shaw <skye.shaw@gmail.com>
-;; Version: 0.7.1
-;; Package-Version: 20181027.426
+;; Version: 0.7.2
+;; Package-Version: 20181031.259
 ;; Keywords: git, vc, github, bitbucket, gitlab, convenience
 ;; URL: http://github.com/sshaw/git-link
 ;; Package-Requires: ((emacs "24.3"))
@@ -36,6 +36,9 @@
 
 ;;; Change Log:
 
+;; 2018-10-30 - v0.7.2
+;; * Fix suffix stripping on remote path only if it ends in .git (Issue #58, thanks Marko Crnic)
+;;
 ;; 2018-07-08 - v0.7.1
 ;; * Add support for vc-revision-other-window files (Issue #54)
 ;;
