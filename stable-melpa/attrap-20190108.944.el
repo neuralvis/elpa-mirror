14a4fa98b52b073e57c7bbaa035de1757d5f654e
Golem <golem@d12frosted.io>
2019-01-08 12:02:18 +0000

    snapshot 08/01/19 12:02:17

--- stable-melpa/attrap-20190108.944.el
+++ stable-melpa/attrap-20190108.944.el
@@ -6,7 +6,7 @@
 ;; Author: Jean-Philippe Bernardy <jeanphilippe.bernardy@gmail.com>
 ;; Maintainer: Jean-Philippe Bernardy <jeanphilippe.bernardy@gmail.com>
 ;; URL: https://github.com/jyp/attrap
-;; Package-Version: 20190105.1026
+;; Package-Version: 20190108.944
 ;; Created: February 2018
 ;; Keywords: programming, tools
 ;; Package-Requires: ((dash "2.12.0") (emacs "25.1") (f "0.19.0") (flycheck "0.30") (s "1.11.0"))
@@ -165,6 +165,7 @@
     "RecordWildCards"
     "ScopedTypeVariables"
     "StandaloneDeriving"
+    "TemplateHaskell"
     "TransformListComp"
     "TupleSections"
     "TypeApplications"
