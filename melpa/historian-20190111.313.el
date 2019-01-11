233f3c11e6de9d4bae0e8e99a1821cf1463e2ed7
Golem <golem@d12frosted.io>
2019-01-11 08:02:31 +0000

    snapshot 11/01/19 08:02:30

--- melpa/historian-20190111.313.el
+++ melpa/historian-20190111.313.el
@@ -4,25 +4,10 @@
 
 ;; Author: PythonNut <pythonnut@pythonnut.com>
 ;; Keywords: convenience
-;; Package-Version: 20180619.1923
+;; Package-Version: 20190111.313
 ;; Version: 20170111
 ;; URL: https://github.com/PythonNut/historian.el
 ;; Package-Requires: ((emacs "24.4"))
-
-;;; License:
-
-;; This program is free software; you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or
-;; (at your option) any later version.
-
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-;; GNU General Public License for more details.
-
-;; You should have received a copy of the GNU General Public License
-;; along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
