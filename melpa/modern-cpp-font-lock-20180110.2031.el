a293d1950cd760175f09623128d5e41f0c53e21c
Golem <golem@d12frosted.io>
2019-01-27 00:02:25 +0000

    snapshot 27/01/19 00:02:24

--- melpa/modern-cpp-font-lock-20180110.2031.el
+++ melpa/modern-cpp-font-lock-20180110.2031.el
@@ -4,7 +4,6 @@
 
 ;; Author: Ludwig PACIFICI <ludwig@lud.cc>
 ;; URL: https://github.com/ludwigpacifici/modern-cpp-font-lock
-;; Package-Version: 20180110.2031
 ;; Version: 0.1.3
 ;; Created: 12 May 2016
 ;; Keywords: languages, c++, cpp, font-lock
