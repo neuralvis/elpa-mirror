6f4d9ae0990ea32308979c3629c8ed900d62aa59
Golem <golem@d12frosted.io>
2019-02-03 04:04:27 +0000

    snapshot 03/02/19 04:04:26

--- melpa/rope-read-mode-20190203.28.el
+++ melpa/rope-read-mode-20190203.28.el
@@ -3,28 +3,29 @@
 ;; THIS FILE HAS BEEN GENERATED.
 
 
-;; Copyright 2015-2017 Marco Wahl
 
+;; Copyright 2015-2019 Marco Wahl
+;; 
 ;; Author: Marco Wahl <marcowahlsoft@gmail.com>
 ;; Maintainer: Marco Wahl <marcowahlsoft@gmail.com>
 ;; Created: 4 Jan 2015
 ;; Version: 0.4.1
-;; Package-Version: 20181224.1100
+;; Package-Version: 20190203.28
 ;; Keywords: reading, convenience, chill
 ;; URL: https://github.com/marcowahl/rope-read-mode
-
+;; 
 ;; This file is not part of Emacs.
-
+;; 
 ;; This program is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
 ;; (at your option) any later version.
-
+;; 
 ;; This program is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
-
+;; 
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
