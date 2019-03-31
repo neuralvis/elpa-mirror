d265e6ce98fb56603c0ecc10bb61ba3057e236b7
Golem <golem@d12frosted.io>
2019-03-31 00:03:54 +0000

    snapshot 31/03/19 00:03:53

--- stable-melpa/format-all-20190330.1906.el
+++ stable-melpa/format-all-20190330.1906.el
@@ -2,11 +2,11 @@
 ;;
 ;; Author: Lassi Kortela <lassi@lassi.io>
 ;; URL: https://github.com/lassik/emacs-format-all-the-code
-;; Package-Version: 20190206.1133
+;; Package-Version: 20190330.1906
 ;; Version: 0.1.0
 ;; Package-Requires: ((emacs "24") (cl-lib "0.5"))
 ;; Keywords: languages util
-;; License: MIT
+;; SPDX-License-Identifier: MIT
 ;;
 ;; This file is not part of GNU Emacs.
 ;;
