2e7f65600957d5246c535cc184cad4ce38526446
Golem <golem@d12frosted.io>
2019-04-21 08:39:26 +0000

    snapshot 21/04/19 08:39:25

--- stable-melpa/js-auto-beautify-20161031.509.el
+++ stable-melpa/js-auto-beautify-20161031.509.el
@@ -2,7 +2,6 @@
 
 ;; Author: quanwei9958@126.com
 ;; Version: 0.0.4
-;; Package-Version: 20161031.509
 ;; Package-Requires: ((web-beautify "0.3.1") (web-mode "14.0.27"))
 
 ;; This program is free software; you can redistribute it and/or modify
