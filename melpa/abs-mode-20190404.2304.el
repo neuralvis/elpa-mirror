ef7531a7ca6a7e26e4c3645a7f6f2106ddb0ccd3
Golem <golem@d12frosted.io>
2019-07-03 12:37:04 +0000

    snapshot 03/07/19 12:37:04

--- melpa/abs-mode-20190404.2304.el
+++ melpa/abs-mode-20190404.2304.el
@@ -4,6 +4,7 @@
 
 ;; Author: Rudi Schlatte <rudi@constantly.at>
 ;; URL: https://github.com/abstools/abs-mode
+;; Package-Version: 20190404.2304
 ;; Version: 1.5
 ;; Package-Requires: ((emacs "25") (erlang "0") (maude-mode "0") (flymake "0.3"))
 ;; Keywords: languages
