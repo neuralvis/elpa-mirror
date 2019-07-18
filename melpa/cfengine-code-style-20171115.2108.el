9aa028aa9dbdfaceae92c74deb5509cd8cc65543
Golem <golem@d12frosted.io>
2019-07-18 20:26:57 +0000

    snapshot 18/07/19 20:26:57

--- melpa/cfengine-code-style-20171115.2108.el
+++ melpa/cfengine-code-style-20171115.2108.el
@@ -2,6 +2,7 @@
 
 ;; Author: Mikhail Gusarov <mikhail.gusarov@cfengine.com>
 ;; URL: https://github.com/cfengine/core
+;; Package-Version: 20171115.2108
 
 ;; This file is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
