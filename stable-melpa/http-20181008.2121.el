ac99aed5f05e96d200d29c7286dc6223f826a0e3
Golem <golem@d12frosted.io>
2019-09-09 00:23:21 +0000

    snapshot 09/09/19 00:23:21

--- stable-melpa/http-20181008.2121.el
+++ stable-melpa/http-20181008.2121.el
@@ -4,6 +4,7 @@
 
 ;; Author: Mario Rodas <marsam@users.noreply.github.com>
 ;; URL: https://github.com/emacs-pe/http.el
+;; Package-Version: 20181008.2121
 ;; Keywords: convenience
 ;; Version: 0.0.1
 ;; Package-Requires: ((emacs "24.4") (request "0.2.0") (edit-indirect "0.1.4"))
