3b19d399bfc96d06da24b145a8fc5955eef654e4
Golem <golem@d12frosted.io>
2019-10-01 08:13:29 +0000

    snapshot 01/10/19 08:13:29

--- stable-melpa/treemacs-evil-20190927.542.el
+++ stable-melpa/treemacs-evil-20190927.542.el
@@ -4,7 +4,7 @@
 
 ;; Author: Alexander Miller <alexanderm@web.de>
 ;; Package-Requires: ((evil "1.2.12") (treemacs "0.0"))
-;; Package-Version: 20190619.1516
+;; Package-Version: 20190927.542
 ;; Package-X-Original-Version: 0
 ;; Homepage: https://github.com/Alexander-Miller/treemacs
 
@@ -81,6 +81,7 @@ Then call ORIG-FUN with its ARGS and reopen treemacs if it was open before."
 (define-key evil-treemacs-state-map (kbd "?")   #'treemacs-helpful-hydra)
 (define-key evil-treemacs-state-map (kbd "RET") #'treemacs-RET-action)
 (define-key evil-treemacs-state-map (kbd "H")   #'treemacs-collapse-parent-node)
+(define-key evil-treemacs-state-map (kbd "!")   #'treemacs-run-shell-command-for-current-node)
 
 (evil-define-key 'treemacs treemacs-mode-map (kbd "yr")     #'treemacs-copy-project-root)
 (evil-define-key 'treemacs treemacs-mode-map (kbd "yy")     #'treemacs-copy-path-at-point)
