5544a6e0a0e6822913c627e6eca39714d4877dc3
Golem <golem@d12frosted.io>
2019-11-05 08:14:54 +0000

    snapshot 05/11/19 08:14:54

--- melpa/julia-repl-20190908.1717.el
+++ melpa/julia-repl-20190908.1717.el
@@ -3,6 +3,7 @@
 ;; Copyright (C) 2016  Tamas K. Papp
 ;; Author: Tamas Papp <tkpapp@gmail.com>
 ;; Keywords: languages
+;; Package-Version: 20190908.1717
 ;; Version: 0.0.1
 ;; Package-Requires: ((emacs "25"))
 ;; URL: https://github.com/tpapp/julia-repl
