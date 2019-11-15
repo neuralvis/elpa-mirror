0acc97a2fbbdaa845f732c2a5a31b7b3cf2c3c2c
Golem <golem@d12frosted.io>
2019-11-15 08:16:13 +0000

    snapshot 15/11/19 08:16:13

--- melpa/color-theme-x-20180227.46.el
+++ melpa/color-theme-x-20180227.46.el
@@ -4,6 +4,7 @@
 ;; Copyright (C) 2012-2018 Andrew Johnson <andrew@andrewjamesjohnson.com>
 
 ;; Version: 1.3
+;; Package-Version: 20180227.46
 ;; Keywords: convenience, faces, frames
 ;; Author: Matthew Kennedy <mkennedy@killr.ath.cx>
 ;; Author: Andrew Johnson <andrew@andrewjamesjohnson.com>
