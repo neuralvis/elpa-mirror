81090fa9e488007f6c056ed9cd9518739ae4aea5
Golem <golem@d12frosted.io>
2019-11-21 16:09:55 +0000

    snapshot 21/11/19 16:09:55

--- melpa/eglot-20191120.2255.el
+++ melpa/eglot-20191120.2255.el
@@ -3,7 +3,6 @@
 ;; Copyright (C) 2018 Free Software Foundation, Inc.
 
 ;; Version: 1.5
-;; Package-Version: 20191120.2255
 ;; Author: João Távora <joaotavora@gmail.com>
 ;; Maintainer: João Távora <joaotavora@gmail.com>
 ;; URL: https://github.com/joaotavora/eglot
