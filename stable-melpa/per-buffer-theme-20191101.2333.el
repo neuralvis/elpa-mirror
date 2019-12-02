82f384d842cf263227f6d09d2d27aa44ff706184
Golem <golem@d12frosted.io>
2019-12-02 00:09:09 +0000

    snapshot 02/12/19 00:09:09

--- stable-melpa/per-buffer-theme-20191101.2333.el
+++ stable-melpa/per-buffer-theme-20191101.2333.el
@@ -4,6 +4,7 @@
 
 ;; Author: Iñigo Serna <inigoserna@gmx.com>
 ;; URL: https://hg.serna.eu/emacs/per-buffer-theme
+;; Package-Version: 20191101.2333
 ;; Version: 1.7
 ;; Keywords: themes
 ;; Package-Requires: ((cl-lib "0.5"))
