d9e4667e29e7bf2310641651e5c9fdb4d4c28ad0
Golem <golem@d12frosted.io>
2019-12-16 16:09:00 +0000

    snapshot 16/12/19 16:09:00

--- stable-melpa/modus-vivendi-theme-20191216.1122.el
+++ stable-melpa/modus-vivendi-theme-20191216.1122.el
@@ -4,7 +4,7 @@
 
 ;; Author: Protesilaos Stavrou <info@protesilaos.com>
 ;; URL: https://gitlab.com/protesilaos/modus-themes
-;; Package-Version: 20191215.845
+;; Package-Version: 20191216.1122
 ;; Version: 0.1.0
 ;; Package-Requires: ((emacs "25.1"))
 ;; Keywords: faces, theme, accessibility
@@ -326,9 +326,9 @@ between foreground and background is >= 7:1)."
    `(avy-lead-face-1 ((,class (:inherit modus-theme-intense-red :weight bold))))
    `(avy-lead-face-2 ((,class (:inherit modus-theme-intense-green :weight bold))))
    ;;;; aw (ace-window)
-   `(aw-background-face ((,class (:background ,bg-alt))))
+   `(aw-background-face ((,class (:background ,bg-dim :foreground ,fg-dim))))
    `(aw-key-face ((,class (:foreground ,blue-intense :weight bold))))
-   `(aw-leading-char-face ((,class (:height 1.5 :inherit modus-theme-intense-green :weight bold))))
+   `(aw-leading-char-face ((,class (:height 1.5 :background ,bg-main :foreground ,red-intense :weight bold))))
    `(aw-minibuffer-leading-char-face ((,class (:foreground ,magenta-active))))
    `(aw-mode-line-face ((,class (:weight bold))))
    ;;;; buttons, links, widgets
