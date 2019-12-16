92d13509a02e0cfde724f4d151a4c2eb78d0340a
Golem <golem@d12frosted.io>
2019-12-16 08:09:05 +0000

    snapshot 16/12/19 08:09:05

--- stable-melpa/gregorio-mode-20170705.1451.el
+++ stable-melpa/gregorio-mode-20170705.1451.el
@@ -5,6 +5,7 @@
 ;; Author: Fr. John Jenkins <jenkins@sspx.ng>
 ;; Created: 2015-10-25
 ;; Version: 1.0
+;; Package-Version: 20170705.1451
 ;; Keywords: gregorio,chant
 ;; URL: https://jsrjenkins.github.io/gregorio-mode/
 
