75695e89814a825077379fb449b5fb7f688d1d7d
Golem <golem@d12frosted.io>
2020-03-15 16:09:03 +0000

    snapshot 15/03/20 16:09:03

--- melpa/import-js-20180709.1833.el
+++ melpa/import-js-20180709.1833.el
@@ -3,7 +3,6 @@
 ;;
 ;; Author: Kevin Kehl <kevin.kehl@gmail.com>
 ;; URL: http://github.com/Galooshi/emacs-import-js/
-;; Package-Version: 20180709.1833
 ;; Package-Requires: ((grizzl "0.1.0") (emacs "24"))
 ;; Version: 1.0.0
 ;; Keywords: javascript
